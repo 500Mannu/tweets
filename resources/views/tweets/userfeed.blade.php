0ec365e88ea3504158b0288a39a299e9e1a5479e
500Mannu <emmkga@gmail.com>
2020-02-17 02:24:03 +0200

    links

--- resources/views/tweets/userfeed.blade.php
+++ resources/views/tweets/userfeed.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
   @foreach($data as $key => $tweet)
-    <a href="user/{{ $tweet->username }}" class="tweet-item">
+    <a href="javascript:void()" class="tweet-item">
       <h4>{{ $tweet->username }} | <span class="meta-date">{{ $tweet->tweet_create_date }}</span></h4>
       <p>{{ $tweet->tweet }}</p> 
     </a>
